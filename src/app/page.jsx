b767fca70c1d7e1c235f31b15b2f4a7addd45a70
Irfan Chowdhury <irfanchowdhury80@gmail.com>
2025-03-14 06:17:35 +0600

    Rgular products

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 export default async function Home() {
 
     const homeData = await getHomeData();
-    const CURRENCY_FORMAT = process.env.CURRENCY_FORMAT; 
-    const SHOW_CURRENCY_SYMBOL = process.env.SHOW_CURRENCY_SYMBOL; 
-    const FORMAT_NUMBER = process.env.FORMAT_NUMBER; 
+    const CURRENCY_FORMAT = process.env.CURRENCY_FORMAT;
+    const SHOW_CURRENCY_SYMBOL = process.env.SHOW_CURRENCY_SYMBOL;
+    const FORMAT_NUMBER = process.env.FORMAT_NUMBER;
 
 
     return (
@@ -18,7 +18,7 @@ export default async function Home() {
                             <div className="col-md-8">
                                 <div className="banner-slider">
                                     {homeData.sliders.map((slider, key) => (
-                                        <div className="item" key={key}> {/* Always add a unique key when mapping */}
+                                        <div className="item" key={key}>
                                             {slider?.slider_image && (
                                                 <div
                                                     className="img-fill"
@@ -86,6 +86,142 @@ export default async function Home() {
                 </div>
             </div>
 
+            {/* Product area starts */}
+            <section className="product-tab-section">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12 text-center">
+                            <ul className="nav nav-tabs product-details-tab" id="lionTab" role="tablist">
+                                {homeData.productDetailsTab.productTabs.map((item, key) => (
+                                    <li className="nav-item" key={key}>
+                                        <a className={key === 0 ? "nav-link active" : "nav-link"} data-bs-toggle="tab" href={item.key} role="tab" aria-selected="true">{item.title}</a>
+                                    </li>
+                                ))}
+                            </ul>
+                            <div className="product-navigation">
+                                <div className="product-button-next v1"><i className="ti-angle-right"></i></div>
+                                <div className="product-button-prev v1"><i className="ti-angle-left"></i></div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="tab-content mt-3" id="lionTabContent">
+                                {homeData.productDetailsTab.productTabs.map((item, key) => (
+                                    <div key={key} className={`tab-pane fade ${key === 0 ? "show active" : ""}`} id={item.key} role="tabpanel" aria-labelledby="all-tab">
+                                        <div class="product-slider-wrapper swiper-container">
+                                            <div class="swiper-wrapper">
+                                                {item.categoryWithProducts.products.map((product, index) => (
+                                                    <div class="swiper-slide">
+                                                        <form key={index} action="/" className="addToCart" method="post">
+                                                            <input type="hidden" name="product_id" value={product.productId} />
+                                                            <input type="hidden" name="product_slug" value={product.slug} />
+                                                            <input type="hidden" name="category_id" value={item.categoryWithProducts.categoryId} />
+                                                            <input type="hidden" name="qty" value="1" />
+
+                                                                <div className="single-product-wrapper">
+                                                                    <div className="single-product-item">
+                                                                        <a className="product-name" href={`${product.slug}/${item.categoryWithProducts.categoryId}`}>
+                                                                            <Image
+                                                                                alt="Product Image"
+                                                                                src={product.mediumImage}
+                                                                                className="lazy"
+                                                                                width={720}
+                                                                                height={660}
+                                                                            />
+                                                                        </a>
+
+                                                                        {product.manageStock === 1 && product.qty === 0 || product.inStock === 0 ? (
+                                                                            <div className="product-promo-text style1 bg-danger">
+                                                                                <span>Stock Out</span>
+                                                                            </div>
+                                                                        ) : new Date() <= new Date(product.newTo) ? (
+                                                                            <div className="product-promo-text style1">
+                                                                                <span>New</span>
+                                                                            </div>
+                                                                        ) : null}
+
+                                                                        <div className="product-overlay">
+                                                                            <a href="#" data-bs-toggle="modal" data-bs-target={`#quickshopTrend_${product.slug}`}> <span className="ti-zoom-in" data-bs-toggle="tooltip" data-bs-placement="top" title="quick view"></span></a>
+                                                                            <a>
+                                                                                <span className="ti-heart add_to_wishlist"
+                                                                                    data-product_id={product.slug}
+                                                                                    data-product_slug={product.slug}
+                                                                                    data-category_id={item.categoryWithProducts.categoryId}
+                                                                                    data-qty="1"
+                                                                                    data-bs-toggle="tooltip"
+                                                                                    data-bs-placement="top"
+                                                                                    title="Add to wishlist">
+                                                                                </span>
+                                                                            </a>
+                                                                        </div>
+
+                                                                        <div className="product-details">
+                                                                            <a className="product-name" href={`${product.slug}/${item.categoryWithProducts.categoryId}`}>
+                                                                                {product.name}
+                                                                            </a>
+                                                                        </div>
+                                                                        <div className="d-flex justify-content-between align-items-center">
+                                                                            <div>
+                                                                                <ul className="product-rating">
+                                                                                    {[...Array(5)].map((_, i) => (
+                                                                                        <li key={i}>
+                                                                                            <i className={i < Math.round(product.avgRating) ? "las la-star" : "lar la-star"}></i>
+                                                                                        </li>
+                                                                                    ))}
+                                                                                </ul>
+                                                                                <div className="product-price">
+                                                                                    {product.specialPrice !== null && product.specialPrice > 0 && product.specialPrice < product.price ? (
+                                                                                        <>
+                                                                                            <span className="old-price">
+                                                                                                {CURRENCY_FORMAT === 'suffix' ?
+                                                                                                    `${(product.price * homeData.changeCurrencyRate).toFixed(FORMAT_NUMBER)} ${SHOW_CURRENCY_SYMBOL}` :
+                                                                                                    `${SHOW_CURRENCY_SYMBOL} ${(product.price * homeData.changeCurrencyRate).toFixed(FORMAT_NUMBER)}`
+                                                                                                }
+                                                                                            </span>
+                                                                                            <span className="promo-price">
+                                                                                                {CURRENCY_FORMAT === 'suffix' ?
+                                                                                                    `${(product.specialPrice * homeData.changeCurrencyRate).toFixed(FORMAT_NUMBER)} ${SHOW_CURRENCY_SYMBOL}` :
+                                                                                                    `${SHOW_CURRENCY_SYMBOL} ${(product.specialPrice * homeData.changeCurrencyRate).toFixed(FORMAT_NUMBER)}`
+                                                                                                }
+                                                                                            </span>
+                                                                                        </>
+                                                                                    ) : (
+                                                                                        <span className="promo-price">
+                                                                                            {CURRENCY_FORMAT === 'suffix' ?
+                                                                                                `${(product.price * homeData.changeCurrencyRate).toFixed(FORMAT_NUMBER)} ${SHOW_CURRENCY_SYMBOL}` :
+                                                                                                `${SHOW_CURRENCY_SYMBOL} ${(product.price * homeData.changeCurrencyRate).toFixed(FORMAT_NUMBER)}`
+                                                                                            }
+                                                                                        </span>
+                                                                                    )}
+                                                                                </div>
+                                                                            </div>
+                                                                            <div>
+                                                                                {(product.manageStock == 1 && product.qty == 0) || product.inStock == 0 ? (
+                                                                                    <span className="d-inline-block" tabIndex="0" data-bs-toggle="tooltip" data-bs-placement="top" title="Disabled"><button className="btn button style2 sm" disabled><i className="las la-cart-plus"></i></button></span>
+                                                                                ) : (
+                                                                                    <button className="button style2 sm" type="submit" data-bs-toggle="tooltip" data-bs-placement="top"><i className="las la-cart-plus"></i></button>
+                                                                                )}
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                        </form>
+                                                    </div>
+                                                ))}
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+
             {/* Trending Start */}
             <section className="product-tab-section">
                 <div className="container">
@@ -98,7 +234,7 @@ export default async function Home() {
                     </div>
                     <div className="row">
                         <div className="product-grid">
-                            {homeData.orderDetailProducts.map((product, key) => (
+                            {homeData.trendProducts.map((product, key) => (
                                 product.isActive && (
                                     <form key={key} action="/" className="addToCart" method="post">
                                         <input type="hidden" name="product_id" value={product.productId} />
@@ -119,11 +255,11 @@ export default async function Home() {
                                                         />
                                                     </a>
 
-                                                    {homeData.orderDetailProducts.manageStock === 1 && homeData.orderDetailProducts.qty === 0 || homeData.orderDetailProducts.inStock === 0 ? (
+                                                    {product.manageStock === 1 && product.qty === 0 || product.inStock === 0 ? (
                                                         <div className="product-promo-text style1 bg-danger">
                                                             <span>Stock Out</span>
                                                         </div>
-                                                    ) : new Date() <= new Date(homeData.orderDetailProducts.newTo) ? (
+                                                    ) : new Date() <= new Date(product.newTo) ? (
                                                         <div className="product-promo-text style1">
                                                             <span>New</span>
                                                         </div>
@@ -169,7 +305,7 @@ export default async function Home() {
                                                                         </span>
                                                                         <span className="promo-price">
                                                                             {CURRENCY_FORMAT === 'suffix' ?
-                                                                                `${(product.specialPrice * homeData.changeCurrencyRate).toFixed(FORMAT_NUMBER)} ${SHOW_CURRENCY_SYMBOL}` : 
+                                                                                `${(product.specialPrice * homeData.changeCurrencyRate).toFixed(FORMAT_NUMBER)} ${SHOW_CURRENCY_SYMBOL}` :
                                                                                 `${SHOW_CURRENCY_SYMBOL} ${(product.specialPrice * homeData.changeCurrencyRate).toFixed(FORMAT_NUMBER)}`
                                                                             }
                                                                         </span>
@@ -185,10 +321,10 @@ export default async function Home() {
                                                             </div>
                                                         </div>
                                                         <div>
-                                                            {(product.manageStock==1 && product.qty==0) || product.inStock==0 ? (
-                                                                <span class="d-inline-block" tabindex="0" data-bs-toggle="tooltip" data-bs-placement="top" title="Disabled"><button class="btn button style2 sm" disabled><i class="las la-cart-plus"></i></button></span>
+                                                            {(product.manageStock == 1 && product.qty == 0) || product.inStock == 0 ? (
+                                                                <span className="d-inline-block" tabIndex="0" data-bs-toggle="tooltip" data-bs-placement="top" title="Disabled"><button className="btn button style2 sm" disabled><i className="las la-cart-plus"></i></button></span>
                                                             ) : (
-                                                                <button class="button style2 sm" type="submit" data-bs-toggle="tooltip" data-bs-placement="top"><i class="las la-cart-plus"></i></button>
+                                                                <button className="button style2 sm" type="submit" data-bs-toggle="tooltip" data-bs-placement="top"><i className="las la-cart-plus"></i></button>
                                                             )}
                                                         </div>
                                                     </div>
